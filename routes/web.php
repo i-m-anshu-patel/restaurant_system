f151e03b407d3e8ea3b5ff37cd1cf9c6a76ecb88
= <=>
2022-07-29 03:15:38 +0530

    Added Admin Template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Log\Logger;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,6 +17,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
+});
+
+Route::get('/adminpanel', function () {
+    if(Auth::check()){
+        Logger(Auth::user()->name);
+        return view('adminpage');
+    }
+    else{
+        return "Access denied";
+    }
+    
 });
 
 Route::middleware([
